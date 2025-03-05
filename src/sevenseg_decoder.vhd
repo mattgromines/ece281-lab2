eb29eaae486ff157a401b291674b71e07304c846
mattgromines <mattgromines@gmail.com>
2025-03-04 20:53:20 -0700

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   Test Bench Waveform.png
    	modified:   src/Basys3_Master.xdc
    	modified:   src/sevenseg_decoder.vhd
    	modified:   src/sevenseg_decoder_tb.vhd
    	modified:   src/top_basys3.vhd

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -38,25 +38,26 @@ end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
     signal o_seg : std_logic_vector (6 downto 0);
-    signal flip : std_logic_vector (6 downto 0);
 begin
     with i_Hex select
-    o_seg <= "0111111" when x"0",
-             "0000110" when x"1",
-             "1011011" when x"2",
-             "1001111" when x"3",
-             "1100110" when x"4",
-             "1101101" when x"5",
-             "1111101" when x"6",
-             "0000111" when x"7",
-             "1111111" when x"8",
-             "1100111" when x"9",
-             "1110111" when x"A",
-             "1111100" when x"B",
-             "1011000" when x"C",
-             "1011110" when x"D",
-             "1111001" when x"E",
-             "1110001" when x"F",
+    -- manually fliped the bit order to accommodate the basys3
+    -- there is probably a more efficient way to do this
+    o_seg <= "0111111" when "0000",
+             "0000110" when "0001",
+             "1011011" when "0010",
+             "1001111" when "0011",
+             "1100110" when "0100",
+             "1101101" when "0101",
+             "1111101" when "0110",
+             "0000111" when "0111",
+             "1111111" when "1000",
+             "1100111" when "1001",
+             "1110111" when "1010",
+             "1111100" when "1011",
+             "1011000" when "1100",
+             "1011110" when "1101",
+             "1111001" when "1110",
+             "1110001" when "1111",
              "0000000" when others;
              
      o_seg_n <=  NOT(o_seg);
--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -1,4 +1,4 @@
-----------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
 -- 
@@ -19,8 +19,9 @@
 ----------------------------------------------------------------------------------
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,13 +33,50 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder_tb is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder_tb;
 
-architecture Behavioral of sevenseg_decoder_tb is
+architecture test_bench of sevenseg_decoder_tb is
+    component sevenseg_decoder is 
+        port(
+        i_Hex : in std_logic_vector(3 downto 0);
+        o_seg_n : out std_logic_vector(6 downto 0)
+        );
+    end component;
+    
+    signal w_sw : std_logic_vector(3 downto 0):= (others=> '0');
+    signal w_seg: std_logic_vector(6 downto 0);
 
-begin
+    begin
+        sevenseg_decoder_inst : sevenseg_decoder port map(
+            i_Hex(0) => w_sw(0),
+            i_Hex(1) => w_sw(1),
+            i_Hex(2) => w_sw(2),
+            i_Hex(3) => w_sw(3),
+            o_seg_n(0) => w_seg(0),
+            o_seg_n(1) => w_seg(1),
+            o_seg_n(2) => w_seg(2),
+            o_seg_n(3) => w_seg(3),
+            o_seg_n(4) => w_seg(4),
+            o_seg_n(5) => w_seg(5),
+            o_seg_n(6) => w_seg(6)
+        );
+        
+    test_process : process
+    begin 
+        -- tests a portion of cases
+        w_sw <= x"0"; wait for 10 ns;
+            assert w_seg = "1000000" report "error on x0" severity failure;
+        w_sw <= x"3"; wait for 10 ns;
+            assert w_seg = "0110000" report "error on x3" severity failure;
+        w_sw <= x"A"; wait for 10 ns;
+            assert w_seg = "0001000" report "error on xA" severity failure;
+        w_sw <= x"F"; wait for 10 ns;
+            assert w_seg = "0001110" report "error on xF" severity failure;
+        w_sw <= x"8"; wait for 10 ns;
+            assert w_seg = "0000000" report "error on x8" severity failure;
+   
+        wait;
+        
+    end process;
 
-
-end Behavioral;
+end test_bench;
